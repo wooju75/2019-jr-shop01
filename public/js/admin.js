d4bac4572d98952f96914d532d0b87dd438d7ccd
wooju75 <dldnwn0517@naver.com>
2019-01-14 11:53:34 +0900

    home 관리자 모드 완료

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,4 @@
+// database = CRUD(Create/Read/Update/Delete)
 // Fidebase Init
 var config = {
 	apiKey: "AIzaSyDIzp4n1G1f_Xa_DVqhxeojAWCjcDPLHwE",
@@ -15,37 +16,55 @@ var ref = new Array();
 var key = "";
 
 // 네비게이션 구현
-
 $(".navs li").on("click", function(){
 	$(".navs li").css({"color":"#aaa", "background-color":"transparent"});
 	$(this).css({"color":"#fff", "background-color":"#444"});
 });
 $(".navs li").eq(0).trigger("click");
 
+// 페이지 시작과 동시에 homeInit()실행하고, FB이벤트 콜백함수를 생성한다.
 homeInit();
 function homeInit() {
 	ref[0] = db.ref("root/home/");
 	ref[0].on("child_added", homeAdd);
+	ref[0].on("child_removed", homeRev);
+	ref[0].on("child_changed", homeChg);
 }
+/***** Firebase Callback 함수.시작 *****/
+// Home 데이터가 추가되면 data변수에 추가된 내용을 담아 실행한다.
 function homeAdd(data){
 	var sel = ["", ""];
 	if(data.val().target == "_blank") sel[0] = "selected";
 	else sel[1] = "selected"; 
 	var html = `
-	<li>
-		<input type="text" class="w3-input w3-border w3-show-inline-block" 
+	<li id="${data.key}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block home_title" 
 		value="${data.val().title}">
-		<input type="text" class="w3-input w3-border w3-show-inline-block" 
+		<input type="text" class="w3-input w3-border w3-show-inline-block home_link" 
 		value="${data.val().link}">
-		<select class="w3-select w3-border w3-show-inline-block">
+		<select class="w3-select w3-border w3-show-inline-block home_target">
 			<option value="_blank" ${sel[0]}>새창</option>
 			<option value="_self"  ${sel[1]}>현재창</option>
 		</select>
-		<button class="w3-green w3-button">수정</button>
-		<button class="w3-red w3-button">삭제</button>
+		<button class="w3-green w3-button" onclick="dataChg(this);">수정</button>
+		<button class="w3-red w3-button" onclick="dataRev(this);">삭제</button>
 	</li>`;
 	$("#homes").append(html);
 }
+// Home 데이터가 삭제되면 data변수에 삭제된 내용을 담아 실행한다.
+function homeRev(data) {
+	$("#"+data.key).remove();
+}
+// Home 데이터가 수정되면 data변수에 수정된 내용을 담아 실행한다.
+function homeChg(data) {
+	$("#"+data.key).stop().animate({"opacity":0}, 300, function(){
+		$(this).css({"opacity":1});
+	});
+}
+/***** Firebase Callback 함수.종료 *****/
+
+
+// home 저장 버튼을 눌렀을때.
 $("#bt1_save").on("click", function(){
 	if($("#title1").val() == "") {
 		alert("제목 !!");
@@ -55,6 +74,7 @@ $("#bt1_save").on("click", function(){
 		alert("링크 !!");
 		return;
 	}
+	//Firebase 추가 명령
 	ref[0].push({
 		title: $("#title1").val(),
 		link: $("#link1").val(),
@@ -63,7 +83,33 @@ $("#bt1_save").on("click", function(){
 	$("#title1").val("");
 	$("#link1").val("");
 });
-
+// home 삭제버튼을 눌렀을때
+function dataRev(obj) {
+	if(confirm("정말로 삭제하시겠습니까?")) {
+		//Firebase 삭제 명령
+		db.ref("root/home/"+$(obj).parent().attr("id")).remove();
+	}
+}
+// home 수정버튼을 눌렀을때
+function dataChg(obj) {
+	var $li = $(obj).parent();
+	key = $li.attr("id");
+	console.log(key); 
+	if($li.find(".home_title").val() == "") {
+		alert("제목 !!");
+		return;
+	}
+	if($li.find(".home_link").val() == "") {
+		alert("링크 !!");
+		return;
+	}
+	//Firebase 수정 명령
+	db.ref("root/home/"+key).update({
+		title: $li.find(".home_title").val(),
+		link: $li.find(".home_link").val(),
+		target: $li.find(".home_target").val()
+	});
+}
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,16 @@ var config = {
 	messagingSenderId: "428495628233"
 };
 firebase.initializeApp(config);
-
+//Firebase Init
+var db = firebase.database();
+db.ref("root/home").on("child_added", homeAdd);
+function homeAdd(data){ //homeAdd의 data를 받아서
+ var html = `
+ <li>
+  <a href="${data.val().link}" target="${data.val().target}">${data.val().title}</a>
+ </li>`;
+ $(".nav_sub").eq(0).append(html);
+}
 
 //top_nav hover이벤트
 $(".top_icon").mouseenter(function(){
@@ -16,6 +25,16 @@ $(".top_icon").mouseenter(function(){
 });
 $(".top_icon").mouseleave(function(){
 	$(this).children("img").css({"opacity":1});
+});
+
+//nav 이벤트 (nav_sub show/hide)
+$(".nav").mouseenter(function(){
+ $(this).children(".nav_sub").css({"display":"block", "opacity":0}).stop().animate({"opacity":1, "top":"45px"}); //.nav에 mouseenter하면 이것의 자식중.nav_sub의 css를 display:block, opacity:0으로 만들고 이벤트를 멈춘다음 animate를 실행한다
+});
+$(".nav").mouseleave(function(){
+  $(this).children(".nav_sub").stop().animate({"opacity":0, "top":"80px"},200, function(){//현재 보여지고 있는 애들은 사라지게 한다
+		$(this).css({"display":"none"});
+	 });
 });
 
 //rt_wings 이벤트
