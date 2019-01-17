560c5962b0ef60f1798e6388664fa7120670381f
wooju75 <dldnwn0517@naver.com>
2019-01-17 14:37:52 +0900

    blog 만드는 중

--- public/js/blog.js
+++ public/js/blog.js
@@ -38,32 +38,39 @@ function init() {
 function onAdd(data) {
 	db.ref("root/blog").limitToLast(1).once("value", function(snapshot){
 		//forEach 는 jQuery each 와 똑같다.
-		snapshot.forEach(function(v){
+		snapshot.forEach(function(v, i){
 			lastKey = v.key;
 		});
 	});
 	$("#mc_sel").prepend(`<option value="${data.key}">${data.val().name}</option>`);
-	$("#mc_sel option").each(function(){
-		if($(this).attr("value") == lastKey) {
-			$(this).attr("selected", true);
-			var html = `
-			<input type="text" id="mc_chg" class="w3-input w3-border w3-show-inline-block" value="${data.val().name}">
-			<button class="w3-button w3-green mc_chg" onclick="mcChg(this);">수정</button>
-			<button class="w3-button w3-red mc_rev" onclick="mcRev(this);">삭제</button>
-			<input type="text" id="sc_in" class="w3-input w3-border w3-show-inline-block" placeholder="서브카테고리 이름">
-			<button id="sc_save" class="w3-button w3-indigo">저장</button>
-			`;
-			$(".cont_wrap").empty().append(html);
-		}
-		else  $(this).attr("selected", false);
-	});
 	$("#mc_sel").trigger("change");
 }
 function onRev(data) {
-
+	$("#mc_sel option:selected").remove();
+	$("#mc_sel").trigger("change");
 }
 function onChg(data) {
-
+	$("#mc_sel option").each(function(){
+		if(data.key == $(this).val()) $(this).text(data.val().name);
+	});
+}
+function mcRev(obj) {
+	if(confirm("정말 삭제? 진심?")) {
+		db.ref("root/blog/"+$("#mc_sel option:selected").val()).remove();
+	}
+}
+function mcChg(obj) {
+	key = $("#mc_sel option:selected").val();
+	log(key);
+	var newTit = $(obj).prev().val();
+	if(newTit == "") {
+		alert("제목을 넣으세요.");
+		$(obj).prev().focus();
+		return;
+	}
+	db.ref("root/blog/"+key).update({
+		name: newTit
+	});
 }
 
 $("#mc_save").click(function(){
@@ -80,5 +87,127 @@ $("#mc_save").click(function(){
 });
 
 $("#mc_sel").change(function(){
-	$("#mc_chg").val($(this).children("option:selected").text());
+
+	$(this).children("option").each(function(){
+		if($(this).val() == lastKey) {
+			$(this).attr("selected", true);
+			var html = `
+			<input type="text" id="mc_chg" class="w3-input w3-border w3-show-inline-block" 
+			value="${$(this).val()}">
+			<button class="w3-button w3-green mc_chg" onclick="mcChg(this);">수정</button>
+			<button class="w3-button w3-red mc_rev" onclick="mcRev(this);">삭제</button>
+			<div class="sc_ins">
+				<input type="text" id="sc_name" class="w3-input w3-border w3-show-inline-block" placeholder="서브카테고리 이름">
+				<input type="text" id="sc_link" class="w3-input w3-border w3-show-inline-block" placeholder="서브카테고리 링크">
+				<select id="sc_target" class="w3-select w3-border w3-show-inline-block">
+					<option value="_blank">새창</option>
+					<option value="_self">현재창</option>
+				</select>
+				<button class="w3-button w3-indigo" onclick="scAdd(this)">저장</button>
+			</div>
+			<ul id="blogs">
+			</ul>`;
+			$(".cont_wrap").empty().append(html);
+		}
+	});
+	$("#mc_chg").val(	$(this).children("option:selected").text()	);
+	db.ref("root/blog/"+key+"/sub").off();
+	key = $(this).children("option:selected").val();
+	db.ref("root/blog/"+key+"/sub").on("child_added", onSubAdd);
+	db.ref("root/blog/"+key+"/sub").on("child_removed", onSubRev);
+	db.ref("root/blog/"+key+"/sub").on("child_changed", onSubChg);
 });
+
+/***** 서브카테고리 *****/
+function scAdd(obj){
+	log("저장!");
+	ref = db.ref("root/blog/"+key+"/sub");
+	if($("#sc_name").val() == "") {
+		alert("제목을 입력하세요.");
+		$("#sc_name").focus();
+		return ;
+	}
+	if($("#sc_link").val() == "") {
+		alert("링크를 입력하세요.");
+		$("#sc_link").focus();
+		return ;
+	}
+	ref.push({
+		name: $("#sc_name").val(),
+		target: $("#sc_target").val(),
+		link: $("#sc_link").val()
+	}).key;
+	$("#sc_name").val("");
+	$("#sc_link").val("");
+}
+function scChg(obj) {
+	var scKey = $(obj).parent().attr("id");
+	var $name = $(obj).prev().prev().prev();
+	var $link = $(obj).prev().prev();
+	var $target = $(obj).prev();
+	if($name.val() == "") {
+		alert("제목이 없습니다.");
+		$name.focus();
+		return;
+	}
+	if($link.val() == "") {
+		alert("링크가 없습니다.");
+		$link.focus();
+		return;
+	}
+	db.ref("root/blog/"+key+"/sub/"+scKey).update({
+		name: $name.val(),
+		link: $link.val(),
+		target: $target.val()
+	});
+}
+function scRev(obj) {
+	if(confirm("정말로 삭제하시겠습니까?")) {
+		var scKey = $(obj).parent().attr("id");
+		db.ref("root/blog/"+key+"/sub/"+scKey).remove();
+	}
+}
+function onSubAdd(data) {
+	var sel = ["", ""];
+	if(data.val().target == "_blank") sel[0] = "selected";
+	else sel[1] = "selected"; 
+	var html = `
+	<li id="${data.key}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block blog_title" 
+		value="${data.val().name}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block blog_link" 
+		value="${data.val().link}">
+		<select class="w3-select w3-border w3-show-inline-block blog_target">
+			<option value="_blank" ${sel[0]}>새창</option>
+			<option value="_self"  ${sel[1]}>현재창</option>
+		</select>
+		<button class="w3-green w3-button" onclick="scChg(this);">수정</button>
+		<button class="w3-red w3-button" onclick="scRev(this);">삭제</button>
+	</li>`;
+	$("#blogs").append(html);
+}
+function onSubRev(data) {
+	$("#"+data.key).remove();
+}
+function onSubChg(data) {
+	$("#"+data.key).stop().animate({"opacity":0}, 300, function(){
+		$(this).css({"opacity":1});
+	});
+}
+
+
+/*
+var arr = new Array("a", "b", "c");
+var arr = ["a", "b", "c"];
+for(var i=0; i<arr.length; i++) {
+	log(arr[i]);
+}
+for(var i in arr) {
+	log(arr[i]);
+}
+arr.forEach(function(value, index){
+	log(value, index);
+});
+$(".abc").each(function(index){
+});
+*/
--- public/js/main.js
+++ public/js/main.js
@@ -14,96 +14,115 @@ var db = firebase.database();
 
 //카테고리 HOME 생성
 db.ref("root/home").on("child_added", homeAdd);
-function homeAdd(data){ //homeAdd의 data를 받아서
- var html = `
- <li class="rt_arrow">
-  <a href="${data.val().link}" target="${data.val().target}">${data.val().title}</a>
- </li>`;
- $(".nav_sub").eq(0).append(html);
+function homeAdd(data) {
+	var html = `
+	<li class="rt_arrow">
+		<a href="${data.val().link}" target="${data.val().target}">${data.val().title}</a>
+	</li>`;
+	$(".nav_sub").eq(0).append(html);
 }
+//카테고리 BLOG 생성
+db.ref("root/blog").on("child_added", blogAdd);
+function blogAdd(data) {
+	var html = `<ul id="${data.key}" class="grid-item">
+		<li class="grid-tit">${data.val().name}</li>
+	</ul>`;
+	$(".grid").append(html);
+}
+/*
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+*/
 
 // 카테고리 SHOP 생성 - Ajax/json 통신
 $.ajax({
-	type: "get", //get 타입은 문서상에서 안써도 자동으로 기본이 get타입이다
-	url: "../json/shop.json", //나의 상위 폴더 주소
-	data: {}, //안보내니까 생략가능
-	dataType: "json", //데이터 타입은 제이슨방식으로 내가 보내겠다
-	success: function(data) {
+	type: "get",
+	url: "../json/shop.json",
+	dataType: "json",
+	success: function (data) {
 		var html = `<div class="shop_cates wrap clear">`;
-		for(var i=0; i<data.cates.length; i++){
-			html += `<ul>
-			<li class="shop_cate_tit">${data.cates[i].tit}</li>
-			<li>
-				<ul class="shop_cate_names">`;
-				for(var j=0; j<data.cates[i].data.length; j++) {
-					html += `
-					<li class="shop_cate_name rt_arrow">
-					<a href="${data.cates[i].data[j].link}" target="${data.cates[i].data[j].target}">
-					${data.cates[i].data[j].name}</a>
-					</li>`;
-				}
-			html +=	`</ul></li></ul>`;
+		for(var i=0; i<data.cates.length; i++) {
+			html += `
+			<ul>
+				<li class="shop_cate_tit">${data.cates[i].tit}</li>
+				<li>
+					<ul>`;
+			for(var j=0; j<data.cates[i].data.length; j++) {
+				html += `
+				<li class="shop_cate_name rt_arrow">
+				<a href="${data.cates[i].data[j].link}" target="${data.cates[i].data[j].target}">
+				${data.cates[i].data[j].name}</a>
+				</li>`;
+			}
+			html += `
+					</ul>
+				</li>
+			</ul>`;
 		}
-		html += `</div><ul class="shop_prds wrap clear">`;
-		for(var i=0; i<data.prds.length; i++) {
-			html += `<li class="shop_prd">
-				<a href="${data.prds[i].link}" target="${data.prds[i].target}">
-				<img src="${data.prds[i].src}" class="img">
-				</a>
+		html += `</div>`;
+		html += `<ul>`;
+		for(i=0; i<data.prds.length; i++) {
+			html += `
+			<li class="shop_prd"><a href="${data.prds[i].link}" target="${data.prds[i].target}">
+			<img src="${data.prds[i].src}" class="img">
+			</a>
 			</li>`;
 		}
 		html += `</ul>`;
 		$(".nav_sub").eq(1).append(html);
-	},
-	error: function(xhr) {
-		log(xhr);
 	}
 });
 
-
-// window.resize()구현 / .nav_sub 위치 잡기
+// window.resize()구현 
 $(window).resize(function(){
-	$(".nav_sub").each(function(){
-		
-	});
+	
 }).trigger("resize");
 
-//top_nav hover이벤트
+
+// top_nav hover 이벤트
 $(".top_icon").mouseenter(function(){
- $(this).children("img").css({"opacity":.7});
+	$(this).children("img").css({"opacity":.7});
 });
 $(".top_icon").mouseleave(function(){
 	$(this).children("img").css({"opacity":1});
 });
 
-//nav 이벤트 (nav_sub show/hide)
+// nav 이벤트 (nav_sub show/hide)
 $(".nav").mouseenter(function(){
- $(this).children(".nav_sub").css({"display":"block", "opacity":0}).stop().animate({"opacity":1, "top":"45px"},200); //.nav에 mouseenter하면 이것의 자식중.nav_sub의 css를 display:block, opacity:0으로 만들고 이벤트를 멈춘다음 animate를 실행한다
+	$(this).children(".nav_sub").css({"display":"block", "opacity":0}).stop().animate({"opacity":1, "top":"45px"}, 200);
 });
 $(".nav").mouseleave(function(){
-  $(this).children(".nav_sub").stop().animate({"opacity":0, "top":"80px"},200, function(){//현재 보여지고 있는 애들은 사라지게 한다
+	$(this).children(".nav_sub").stop().animate({"opacity":0, "top":"80px"}, 200, function(){
 		$(this).css({"display":"none"});
-	 });
-});
-
-//rt_wings 이벤트
-$(".top_nav .fa-bars").click(function(){
- var $bg = $(".rt_bg");
- var $cont = $(".rt_cont");
- $bg.css({"opacity":0, "display":"block"}).stop().animate({"opacity":.3},1000) /* css를 주고 애니메이트~ */
- $cont.css({"display":"block", "right":"-240px"}).stop().animate({"right":0},1000) /* css를 주고 애니메이트~ */
- });
- 
-$(".rt_cont .fa-close").click(function(){
- var $bg = $(".rt_bg");
- var $cont = $(".rt_cont");
- $bg.stop().animate({"opacity":0},1000, function(){
-	$(this).css({"display":"none"});
 	});
- $cont.stop().animate({"right":"-240px"},1000, function(){
-	$(this).css({"display":"none"});
-	}); 
 });
 
-//메인 네비 / .navs
+// rt_wings 이벤트
+$(".top_nav .fa-bars").click(function(){
+	var $bg = $(".rt_bg");
+	var $cont = $(".rt_cont");
+	$bg.css({"opacity":0, "display":"block"}).stop().animate({"opacity":.3}, 1000);
+	$cont.css({"display":"block", "right":"-240px"}).stop().animate({"right":0}, 1000);
+});
+
+$(".rt_cont .fa-close").click(function(){
+	var $bg = $(".rt_bg");
+	var $cont = $(".rt_cont");
+	$bg.stop().animate({"opacity":0}, 800, function(){
+		$(this).css({"display":"none"});
+	});
+	$cont.stop().animate({"right":"-240px"}, 800, function(){
+		$(this).css({"display":"none"});
+	});
+});
+
+$(".rt_bg").click(function(e){
+	e.stopPropagation();
+	$(".rt_cont .fa-close").trigger("click");
+});
+
+//메인네비 / .navs
 //firebase.database().ref("root/test").push({test:"테스트"}).key;
